dfa7b312fda7f47ce7c33c3e36b8dd4dbcaf9413
ianmckellar <ian.mckellar@socialchorus.com>
2021-08-11 22:52:08 -0600

    refactoring weather.py

--- weather.py
+++ weather.py
@@ -1,15 +1,29 @@
 import pandas as pd
 
-df_temp = pd.read_csv('data_files/tas_mean_per_month_per_country.csv')
-df_happy = pd.read_csv('data_files/WHR2021.csv')
-print(df_temp.columns)
 
-df_temp = df_temp.drop(columns=['iso3'])
-df_temp = df_temp[df_temp['year'] >= 2008]
+class temperature:
+    # def _load_data(self):
+    df_temp = pd.read_csv('data_files/tasmax.csv', usecols=range(5))
 
-df_temp = df_temp.groupby(['year','country']).mean().reset_index()
-#df_temp.groupby(['year','country']).mean().to_csv('year_country_mean.csv')
+    # df_happy = pd.read_csv('data_files/WHR2021.csv')
+    print(df_temp.columns)
+    df_temp = df_temp.rename(
+        columns={' Year': 'year',
+                 ' Statistics': 'statistics',
+                 ' Country': 'country'})
 
-mapped_country_temp = (df_happy.merge(df_temp.rename(columns={'country':'Country name'}), on=['Country name', 'year'], how='left'))
+    df_temp = df_temp.drop(columns=[' ISO3'])
 
-mapped_country_temp.to_csv("testing_mapped.csv")
+    print(df_temp.columns)
+
+    df_temp['country'] = df_temp['country'].str.lstrip()
+
+    df_temp = df_temp[df_temp['year'] >= 2008]
+    df_temp = df_temp.groupby(['year', 'country']).mean().reset_index()
+    # df_temp.groupby(['year','country']).mean().to_csv('year_country_mean.csv')
+    # mapped_country_temp =
+    #     (df_happy.merge(df_temp.rename(columns={'country': 'Country name'}), on=['Country name', 'year'], how='left'))
+
+    print(df_temp)
+
+    df_temp.to_csv("testing_mapped.csv")
