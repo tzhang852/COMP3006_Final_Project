fdeb2f2d17dba3c25da6a640660add140d340c89
ianmckellar <ian.mckellar@socialchorus.com>
2021-08-13 21:48:43 -0600

    working on weather.py

--- weather.py
+++ weather.py
@@ -1,29 +1,70 @@
 import pandas as pd
 
+# data visualization
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
+
 
 class temperature:
     # def _load_data(self):
-    df_temp = pd.read_csv('data_files/tasmax.csv', usecols=range(5))
+    def _load_data(self):
+        return pd.read_csv('data_files/tasmax.csv', usecols=range(5))
 
-    # df_happy = pd.read_csv('data_files/WHR2021.csv')
-    print(df_temp.columns)
-    df_temp = df_temp.rename(
-        columns={' Year': 'year',
-                 ' Statistics': 'statistics',
-                 ' Country': 'country'})
+    def _clean_data(self):
+        df_temp = self._load_data()
+        df_temp = df_temp.drop(columns=[' ISO3'])
+        df_temp = df_temp.rename(columns={' Year': 'year',
+                                          ' Statistics': 'max_temp',
+                                          ' Country': 'country'})
+        df_temp['country'] = df_temp['country'].str.lstrip()
+        df_temp = df_temp[df_temp['year'] >= 2008]
+        df_temp = df_temp.groupby(['year', 'country']).mean().reset_index()
+        df_temp = df_temp.groupby(['year', 'country']).mean()
+        return df_temp
 
+    def temp_map(self):
+        df_temp = self._clean_data()
+        fig = px.choropleth(df_temp,
+                            locations="country",
+                            locationmode='country names',
+                            color="Max Temperature",
+                            hover_name="Country",
+                            range_color=[1, 1000],
+                            color_continuous_scale="blues",
+                            title='Density of Countries in 2020')
+        fig.update(layout_coloraxis_showscale=True)
+        fig.show()
+
+    temp_map()
+
+# def _load_data(self):
+def _load_data(self):
+    return pd.read_csv('data_files/tasmax.csv', usecols=range(5))
+
+def _clean_data(self):
+    df_temp = self._load_data()
     df_temp = df_temp.drop(columns=[' ISO3'])
-
-    print(df_temp.columns)
-
+    df_temp = df_temp.rename(columns={' Year': 'year',
+                                      ' Statistics': 'max_temp',
+                                      ' Country': 'country'})
     df_temp['country'] = df_temp['country'].str.lstrip()
-
     df_temp = df_temp[df_temp['year'] >= 2008]
     df_temp = df_temp.groupby(['year', 'country']).mean().reset_index()
-    # df_temp.groupby(['year','country']).mean().to_csv('year_country_mean.csv')
-    # mapped_country_temp =
-    #     (df_happy.merge(df_temp.rename(columns={'country': 'Country name'}), on=['Country name', 'year'], how='left'))
+    df_temp = df_temp.groupby(['year', 'country']).mean()
+    return df_temp
 
-    print(df_temp)
+def temp_map(self):
+    df_temp = self._clean_data()
+    fig = px.choropleth(df_temp,
+                        locations="country",
+                        locationmode='country names',
+                        color="Max Temperature",
+                        hover_name="Country",
+                        range_color=[1, 1000],
+                        color_continuous_scale="blues",
+                        title='Density of Countries in 2020')
+    fig.update(layout_coloraxis_showscale=True)
+    fig.show()
 
-    df_temp.to_csv("testing_mapped.csv")
+temp_map()
