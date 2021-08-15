604a55ff3859e8e9389491f54f3b93b02da4bba3
ianmckellar <ian.mckellar@socialchorus.com>
2021-08-14 18:00:41 -0600

    weather.py from s3 and AppConfig update

--- AppConfig.py
+++ AppConfig.py
@@ -1,19 +1,22 @@
 import configparser
 
-class AppConfig(object):
 
-	def __init__(self):
-		"""
-			Constructs a new AppConfig instance and populates settings based on appsettings.ini first, then appsettings.development.ini
-		"""
-		read_config(self, "appsettings.development.ini")
+class AppConfig:
+    def __init__(self):
+        """
+            Constructs a new AppConfig instance and populates settings based on appsettings.ini first,
+            then appsettings.development.ini
+        """
+        # self.read_config("appsettings.development.ini")
+        self.bucket, self.accessKeyId, self.secretAccessKey, self.region = self.read_config("appsettings.ini")
 
+    def read_config(self, file):
+        parser = configparser.ConfigParser()
+        parser.read(file)
 
-def read_config(self, file):
-	parser = configparser.SafeConfigParser()
-	parser.read(file)
+        bucket = parser.get('AWS', 'AWS_S3_BUCKET')
+        accessKeyId = parser.get('AWS', 'AWS_ACCESS_KEY_ID')
+        secretAccessKey = parser.get('AWS', 'AWS_SECRET_ACCESS_KEY')
+        region = parser.get('AWS', 'AWS_REGION')
 
-	self.Bucket = parser.get('AWS','AWS_S3_BUCKET')
-	self.AccessKeyId = parser.get('AWS','AWS_ACCESS_KEY_ID')
-	self.SecretAccessKey = parser.get('AWS','AWS_SECRET_ACCESS_KEY')
-	self.Region = parser.get('AWS','AWS_REGION')
+        return (bucket, accessKeyId, secretAccessKey, region)
--- weather.py
+++ weather.py
@@ -1,70 +1,64 @@
 import pandas as pd
-
-# data visualization
-import matplotlib.pyplot as plt
-import seaborn as sns
+import AppConfig
 import plotly.express as px
+import boto3
 
 
-class temperature:
-    # def _load_data(self):
+class Temperature:
+    def __init__(self):
+        self.df = self._clean_data()
+
     def _load_data(self):
+        """
+        Forces 5 columns since some country names contain comma
+
+        :returns dataframe built from weather dataset:
+        """
+        appConfig = AppConfig.AppConfig()
+        session = boto3.Session(aws_access_key_id=appConfig.accessKeyId,
+                                aws_secret_access_key=appConfig.secretAccessKey)
+        file = session.resource('s3').Bucket(appConfig.bucket).download_file('tasmax.csv', "data_files/")
+        print(file)
         return pd.read_csv('data_files/tasmax.csv', usecols=range(5))
 
     def _clean_data(self):
+        """
+        Loads in data _load_data function
+        Drops irrelevant columns, renames columns to more appropriate names, strips from leading spaces from strings,
+        removes data for years prior to 2008 as world happiness dataset only goes back to 2008
+        Takes average over year for each country (the month column is lost in this process)
+
+        :returns cleaned dataset:
+        """
         df_temp = self._load_data()
         df_temp = df_temp.drop(columns=[' ISO3'])
         df_temp = df_temp.rename(columns={' Year': 'year',
-                                          ' Statistics': 'max_temp',
+                                          ' Statistics': 'month',
+                                          'tasmax': 'max_temp',
                                           ' Country': 'country'})
         df_temp['country'] = df_temp['country'].str.lstrip()
         df_temp = df_temp[df_temp['year'] >= 2008]
         df_temp = df_temp.groupby(['year', 'country']).mean().reset_index()
-        df_temp = df_temp.groupby(['year', 'country']).mean()
-        return df_temp
+        return df_temp.reset_index()
 
     def temp_map(self):
-        df_temp = self._clean_data()
-        fig = px.choropleth(df_temp,
+        """
+        Displays an interactive map of all countries
+        User can select year to see heat map for countries in selected year
+        """
+        fig = px.choropleth(self.df,
                             locations="country",
                             locationmode='country names',
-                            color="Max Temperature",
-                            hover_name="Country",
-                            range_color=[1, 1000],
+                            color="max_temp",
+                            hover_name="country",
+                            range_color=[self.df.max_temp.min(), self.df.max_temp.max()],
                             color_continuous_scale="blues",
-                            title='Density of Countries in 2020')
+                            title='Max Temperature by countries from 2008 to 2020',
+                            animation_frame="year")
         fig.update(layout_coloraxis_showscale=True)
         fig.show()
 
-    temp_map()
 
-# def _load_data(self):
-def _load_data(self):
-    return pd.read_csv('data_files/tasmax.csv', usecols=range(5))
-
-def _clean_data(self):
-    df_temp = self._load_data()
-    df_temp = df_temp.drop(columns=[' ISO3'])
-    df_temp = df_temp.rename(columns={' Year': 'year',
-                                      ' Statistics': 'max_temp',
-                                      ' Country': 'country'})
-    df_temp['country'] = df_temp['country'].str.lstrip()
-    df_temp = df_temp[df_temp['year'] >= 2008]
-    df_temp = df_temp.groupby(['year', 'country']).mean().reset_index()
-    df_temp = df_temp.groupby(['year', 'country']).mean()
-    return df_temp
-
-def temp_map(self):
-    df_temp = self._clean_data()
-    fig = px.choropleth(df_temp,
-                        locations="country",
-                        locationmode='country names',
-                        color="Max Temperature",
-                        hover_name="Country",
-                        range_color=[1, 1000],
-                        color_continuous_scale="blues",
-                        title='Density of Countries in 2020')
-    fig.update(layout_coloraxis_showscale=True)
-    fig.show()
-
-temp_map()
+temperature = Temperature()
+print(temperature.df)
+temperature.temp_map()
