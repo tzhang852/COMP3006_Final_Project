6dd264bdb1b449238888527e49bd05ac82b9fbc5
Tiffany Zhang <tzhang852@gmail.com>
2021-08-20 22:15:37 -0700

    analysis update

--- analysis.py
+++ analysis.py
@@ -8,11 +8,21 @@ import matplotlib.pyplot as plt
 import boto3
 import argparse
 import happiness
-import weather
+import temperature
 
 class Analysis:
     def __init__(self):
-        
+        self.temp_df = temperature.Temperature()
+        self.happ_df = happiness.Happiness()
+
     def mean_max_temp(self):
+        #plot mean max temp against average happiness for each country per year
+        #bubble map
+        #combine dataframe
+        temp_happ_df = pd.concat([self.temp_df, self.happ_df], axis = 1)
         
+        # fig = px.scatter_geo(df, locations="iso_alpha", color="continent",
+        #              hover_name="country", size="pop",
+        #              projection="natural earth")
+        # fig.show()
     
